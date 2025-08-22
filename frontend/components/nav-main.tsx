046a7a2ad3891a1fb0f498b8429dcf57ae1ce9ae
Mekin Jemal <mekinjemal999@gmail.com>
2025-08-22 08:58:24 +0300

    Refactor layout and navigation components for improved structure and readability; update metadata for better SEO

--- frontend/components/nav-main.tsx
+++ frontend/components/nav-main.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import {  type LucideIcon } from "lucide-react"
+import { type LucideIcon } from "lucide-react"
 
 import {
   Collapsible,
@@ -17,6 +17,7 @@ import {
   SidebarMenuSubButton,
   SidebarMenuSubItem,
 } from '@/components/ui/sidebar'
+import Link from "next/link"
 
 export function NavMain({
   items,
@@ -44,14 +45,16 @@ export function NavMain({
             className="group/collapsible"
           >
             <SidebarMenuItem>
-              <CollapsibleTrigger asChild>
+              {/* <CollapsibleTrigger asChild> */}
+              <Link href={item.url} className="cursor-pointer">
                 <SidebarMenuButton tooltip={item.title}>
                   {item.icon && <item.icon />}
                   <span>{item.title}</span>
                   {/* <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" /> */}
                 </SidebarMenuButton>
-              </CollapsibleTrigger>
-              <CollapsibleContent>
+              </Link>
+              {/* </CollapsibleTrigger> */}
+              {/* <CollapsibleContent>
                 <SidebarMenuSub>
                   {item.items?.map((subItem) => (
                     <SidebarMenuSubItem key={subItem.title}>
@@ -63,7 +66,7 @@ export function NavMain({
                     </SidebarMenuSubItem>
                   ))}
                 </SidebarMenuSub>
-              </CollapsibleContent>
+              </CollapsibleContent> */}
             </SidebarMenuItem>
           </Collapsible>
         ))}
