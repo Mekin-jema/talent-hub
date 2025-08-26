b66242eea3e1d2d2e280d71a02b6fe6f5a2af750
Mekin Jemal <mekinjemal999@gmail.com>
2025-08-26 17:46:19 +0300

    disable reactStrictMode in Next.js configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: false,
+  // other config here..
   images: {
     remotePatterns: [
       {
@@ -8,7 +10,7 @@ const nextConfig = {
       },
     ],
   },
-  reactStrictMode: true,
+
 };
 
 module.exports = nextConfig;
